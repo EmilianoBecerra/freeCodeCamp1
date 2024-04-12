f5a0a260f8daaef15015de946a1da2767567c760
Emiliano Becerra <emibecerra92@gmail.com>
2024-04-12 11:09:52 -0300

    refactor romanNumber array

--- desafioRomanNumeralConverter/script.js
+++ desafioRomanNumeralConverter/script.js
@@ -2,75 +2,69 @@ const convertBtn = document.getElementById('convert-btn');
 const number = document.getElementById('number');
 const output = document.getElementById('output');
 
-const romanNumber = [
-    {
-        name: 'unit',
-        0: '',
-        1: 'I',
-        2: 'II',
-        3: 'III',
-        4: 'IV',
-        5: 'V',
-        6: 'VI',
-        7: 'VII',
-        8: 'VIII',
-        9: 'IX',
-    },
-    {
-        name: 'tens',
-        0: '',
-        1: 'X',
-        2: 'XX',
-        3: 'XXX',
-        4: 'XL',
-        5: 'L',
-        6: 'LX',
-        7: 'LXX',
-        8: 'LXXX',
-        9: 'XC',
-    },
-    {
-        name: 'hundreds',
-        0: '',
-        1: 'C',
-        2: 'CC',
-        3: 'CCC',
-        4: 'CD',
-        5: 'D',
-        6: 'DC',
-        7: 'DCC',
-        8: 'DCCC',
-        9: 'CM',
-    },
-    {
-        name: 'unit_of_thousand',
-        1: 'M',
-        2: 'MM',
-        3: 'MMM'
+const romanNumber = {
+       unit: {
+            0: '',
+            1: 'I',
+            2: 'II',
+            3: 'III',
+            4: 'IV',
+            5: 'V',
+            6: 'VI',
+            7: 'VII',
+            8: 'VIII',
+            9: 'IX',
+        },
+        tens: {
+                0: '',
+                1: 'X',
+                2: 'XX',
+                3: 'XXX',
+                4: 'XL',
+                5: 'L',
+                6: 'LX',
+                7: 'LXX',
+                8: 'LXXX',
+                9: 'XC',
+            },
+        hundreds:{
+            0: '',
+            1: 'C',
+            2: 'CC',
+            3: 'CCC',
+            4: 'CD',
+            5: 'D',
+            6: 'DC',
+            7: 'DCC',
+            8: 'DCCC',
+            9: 'CM',
+        },
+        unit_thousand: {
+            0: '',
+            1: 'M',
+            2: 'MM',
+            3: 'MMM'
+        }
     }
-
-]
+            
+    
 
 const checkNumber = () => {
-    const stringNum = number.value;
-    const break_down = stringNum.split('');
-    const roman_number_unit = romanNumber[0];
-    const roman_number_tens = romanNumber[1];
-    const roman_number_hundreds = romanNumber[2];
-    const roman_number_unit_thousand = romanNumber[3];
+    const string_num = number.value;
+    const break_down = string_num.split('');
     let text_template;
     
     if(break_down.length === 1){
-        text_template = `${roman_number_unit[break_down[0]]}`
+        text_template = `${romanNumber.unit[break_down[0]]}`
     }
     else if(break_down.length === 2){
-       text_template = `${roman_number_tens[break_down[0]]}${roman_number_unit[break_down[1]]}`
+       text_template = `${romanNumber.tens[break_down[0]]}${romanNumber.unit[break_down[1]]}`
     }
     else if(break_down.length === 3) {
-        text_template = `${roman_number_hundreds[break_down[0]]}${roman_number_tens[break_down[1]]}${roman_number_unit[break_down[2]]}`
+        text_template = `${romanNumber.hundreds[break_down[0]]}${romanNumber.tens[break_down[1]]}${romanNumber.unit[break_down[2]]}`
     }
     else {
-        text_template = `${roman_number_unit_thousand[break_down[0]]}${roman_number_hundreds[break_down[1]]}${roman_number_tens[break_down[2]]}${roman_number_unit[break_down[3]]}`
+        text_template = `${romanNumber.unit_thousand[break_down[0]]}${romanNumber.hundreds[break_down[1]]}${romanNumber.tens[break_down[2]]}${romanNumber.unit[break_down[3]]}`
     }
     output.textContent= text_template;
 }
@@ -78,7 +72,6 @@ const checkNumber = () => {
 
 convertBtn.addEventListener('click',()=>{
     const int = parseInt(number.value);
-    console.log(int)
     if(isNaN(int)){
         output.textContent = "Please enter a valid number";
     }
